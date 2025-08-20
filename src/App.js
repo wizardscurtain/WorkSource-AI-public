0daa9e4d48b4810f168c28b081f94fd2a8f69010
Cecil Jentges <cecil@inreachservices.com>
2025-08-20 11:52:41 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import KabamLandingPage from './KabamLandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <KabamLandingPage />
+    </div>
+  );
+}
+
+export default App;
